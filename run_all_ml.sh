d49b953f21a920bb43de5815f5793b4eddecca51
Ravinder Singh <ravinder.fm.singh@gmail.com>
2018-02-23 16:17:39 -0800

    Added run ML script

---
+++ run_all_ml.sh
@@ -0,0 +1,8 @@
+/home/hadoop/HiBench/bin/workloads/ml/kmeans/prepare/prepare.sh
+/home/hadoop/HiBench/bin/workloads/ml/kmeans/spark/run.sh
+/home/hadoop/HiBench/bin/workloads/ml/bayes/prepare/prepare.sh
+/home/hadoop/HiBench/bin/workloads/ml/bayes/spark/run.sh
+/home/hadoop/HiBench/bin/workloads/ml/rf/prepare/prepare.sh
+/home/hadoop/HiBench/bin/workloads/ml/rf/spark/run.sh
+/home/hadoop/HiBench/bin/workloads/ml/lda/prepare/prepare.sh
+/home/hadoop/HiBench/bin/workloads/ml/lda/spark/run.sh
